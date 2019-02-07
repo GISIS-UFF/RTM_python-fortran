6fef4e87bc6ceca57f9e2579979fc3bc4a48275d
fetim <felipetimoteo@id.uff.br>
2019-02-07 20:30:00 -0200

    Repaired filter_StackSection

--- src/filter_StackSection.sh
+++ src/filter_StackSection.sh
@@ -41,36 +41,37 @@ Hplot=900       # Height of plot (pixels)
 
 plotymax=50 # y axis limit (frequency spectrum)
 
-
 # Data Parameters
-indata=../Imagem/Plano_Paralelo_shot003.bin
+indata=../Imagem/Marmousi_FinalImage.bin
 
 #indata=../Models_true/velocitymodel.bin
 Nz=141
-dh=10.0e-3
+dh=10
 
-#ximage n1=$Nz < $indata
+# sample rate in micro in SU # 
+sampling=`python -c "print(float('$dh')*float('1.0e3'))"`
 
 # Cut Frequency
-f_cut=10
+f_cut=6
 f_cut_max=30
 
-
 # End of filter taper   
-d_fcut=2        
+d_fcut=5
+
 sufix=_SUfilter_
 Hz=Hz
 outdata=$indata$sufix$f_cut-$f_cut_max$Hz.bin
 
 #plot Migrated Seismic Section
-suaddhead < $indata ns=$Nz | suchw key1=dt a=$dh |\
+suaddhead < $indata ns=$Nz | suchw key1=dt a=$sampling |\
             suximage  perc=98 title="Migrated Seismic Section" legend=1 units=" Amplitude  "\
             xbox=10 ybox=10 wbox=$Wplot hbox=$Hplot  legend=1 label1="Depth (km)" label2="Lenth (m)" &
 
-#plot Filtered Frequency Spectrum of Migrated Seismic Section    - BAND PASS
-suaddhead < $indata ns=$Nz | suchw key1=dt a=$dh |\
-           sufilter f=$(($f_cut-$d_fcut)),$f_cut,$f_cut_max,$(($f_cut_max+$d_fcut)) amps=0.,1.,1.,0.|\
-           #suspecfx |#  suop op=norm | suwind tmin=0 tmax=$plotymax|\
+#plot Filtered Frequency Spectrum of Migrated Seismic Section
+suaddhead < $indata ns=$Nz | suchw key1=dt a=$sampling |           
+           sufilter f=$f_cut,$(($f_cut+$d_fcut)),$(($f_cut_max-$d_fcut)),$f_cut_max amps=0.,1.,1.,0.| #BANDPASS FILTER           
+           #sufilter f=$(($f_cut-$d_fcut)),$f_cut amps=1.,0.| #LOWPASS FILTER             
+           suspecfx | #suop op=norm | #SHOW AMPLITUDE SPECTRUM  
            suximage  perc=98 title="Frequency Spectrum of Migrated Seismic Section with band filter with $f_cut and $f_cut_max Hz" \
            xbox=10 ybox=10 wbox=$Wplot hbox=$Hplot  \
            legend=1 label1="Depth (km)" label2="Lenth (m)"&# cmap=hsv2 &
